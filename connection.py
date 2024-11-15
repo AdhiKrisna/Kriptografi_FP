543b0802fb466cec35cb08573f8ce19c1dfcab8f
AdhiKrisna <krisnahmbtn@gmail.com>
2024-11-15 19:06:49 +0700

    update:connection

--- connection.py
+++ connection.py
@@ -1,8 +1,67 @@
 import mysql.connector
 from mysql.connector import Error
+# import psycopg2
+# import pandas as pd
+# import streamlit as st
+
+# # Initialize connection
+# def create_connection():
+#     try:
+#         connection = psycopg2.connect(
+#             host=st.secrets["host"],
+#             user=st.secrets["username"],
+#             password=st.secrets["password"],
+#             database=st.secrets["database"],
+#             port=st.secrets.get("port", 5432)
+#         )
+#         return connection
+#     except Error as e:
+#         st.error(f"Error connecting to MySQL: {e}")
+#         return None
+
+# # Perform query
+# def run_query(query, params=None, fetch=True):
+#     """
+#     Runs a SQL query with optional parameters.
+    
+#     - `query`: The SQL query string.
+#     - `params`: Optional parameters to be passed with the query.
+#     - `fetch`: If True, fetches results (used for SELECT queries).
+#     """
+#     conn = create_connection()
+#     if conn:
+#         cursor = conn.cursor(dictionary=True)
+#         try:
+#             if params:
+#                 cursor.execute(query, params)
+#             else:
+#                 cursor.execute(query)
+            
+#             # For SELECT queries, fetch results
+#             if fetch:
+#                 result = cursor.fetchall()
+#                 return pd.DataFrame(result) if result else pd.DataFrame()
+            
+#             # For INSERT, UPDATE, DELETE queries
+#             else:
+#                 conn.commit()
+#                 return None
+#         except Exception as e:
+#             st.error(f"Error executing query: {e}")
+#             return None
+#         finally:
+#             cursor.close()
+#             conn.close()
+#     else:
+#         st.error("Failed to connect to the database.")
+#         return None
+
+
 import psycopg2
 import pandas as pd
 import streamlit as st
+from psycopg2 import Error
+from psycopg2.extras import DictCursor
 
 # Initialize connection
 def create_connection():
@@ -16,7 +75,7 @@ def create_connection():
         )
         return connection
     except Error as e:
-        st.error(f"Error connecting to MySQL: {e}")
+        st.error(f"Error connecting to PostgreSQL: {e}")
         return None
 
 # Perform query
@@ -30,7 +89,7 @@ def run_query(query, params=None, fetch=True):
     """
     conn = create_connection()
     if conn:
-        cursor = conn.cursor(dictionary=True)
+        cursor = conn.cursor(cursor_factory=DictCursor)  # Gunakan DictCursor di sini
         try:
             if params:
                 cursor.execute(query, params)
